4606bd22930121a93a8fcda694fca347e6437b64
Ferenc Horvay <ferenc.horvay@de.markant.com>
2020-08-12 09:58:12 +0200

    added loglevel support

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	"flag"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
-	initLogger()
+	err := setLogLevel(getCLArgLoglevel())
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
 }
 
-func initLogger()  {
-	log.SetLevel(log.DebugLevel)
+func getCLArgLoglevel() string {
+	loglevel := flag.String("Loglevel",log.DebugLevel.String(),"Set the Loglevel")
+	flag.Parse()
+	return *loglevel
+}
+
+func setLogLevel(loglevel string) error {
+	lvl, err := log.ParseLevel(loglevel)
+	if err != nil {
+		return err
+	}
+	log.SetLevel(lvl)
+	return nil
 }
