27b9b170d868ea73e808e08f67498c0efb5f282f
Ferenc Horvay <ferenc.horvay@de.markant.com>
2020-08-12 09:39:05 +0200

    test for golint

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import "github.com/sirupsen/logrus"
 
 func main() {
 	initLogger()
 }
 
 func initLogger()  {
-	log.SetLevel(log.DebugLevel)
+
 }
