63398b1e8ddb1722c986366625177e8189502ac0
Ferenc Horvay <ferenc.horvay@de.markant.com>
2020-09-10 18:20:36 +0200

    added default test

--- pkg/connection/connection_test.go
+++ pkg/connection/connection_test.go
@@ -1,1 +1,34 @@
 package connection
+
+import (
+	"github.com/bpicode/fritzctl/fritz"
+	"github.com/ferencovonmatterhorn/fritzdect-prom-exporter/pkg/config"
+	"reflect"
+	"testing"
+)
+
+func TestConnectToFritzbox(t *testing.T) {
+	type args struct {
+		credentials config.FritzBoxCredentials
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    fritz.HomeAuto
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ConnectToFritzbox(tt.args.credentials)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ConnectToFritzbox() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ConnectToFritzbox() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
